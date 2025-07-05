266e44eac056ab7df8e3218559b0f05d6698c654
AndrejShylovich <andrejshylovich@gmail.com>
2025-07-05 21:19:46 +0300

    fixed keyboard config

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,21 +1,14 @@
 import { useContext, useEffect } from "react";
 import { CTX } from "../store/Store";
 import QwertyHancock from "qwerty-hancock";
+import { KEYBOARD_CONFIG } from "../utils/constants";
 
 const Keyboard = () => {
   const [, updateState] = useContext(CTX);
 
   useEffect(() => {
     const keyboard = new QwertyHancock({
-      id: "keyboard",
-      width: "1182",
-      height: "100",
-      octaves: 6,
-      startNote: "A2",
-      whiteKeyColour: "rgb(255, 255, 255)",
-      blackKeyColour: "rgb(0, 8, 22)",
-      activeKeyColour: "rgb(166,49,172)",
-      borderColour: "",
+      ...KEYBOARD_CONFIG
     });
     keyboard.keyDown = (note, freq) => {
       updateState({ type: "MAKE_OSC", payload: { note, freq } });
